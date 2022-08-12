7bc70633ce5aa97cd619881a66dc7215c4a98167
achille <achille.verheye@gmail.com>
2022-08-11 21:08:10 -0700

    Disable 3D, make 2D work again

--- execute_path.py
+++ execute_path.py
@@ -22,25 +22,24 @@ theta2, theta3, rho, zee = symbols('theta2 theta3 rho zee')
 kinematics = Manipulator2()
 
 # visualize
-viz = CuspidalVisualizer(kinematics)
+viz = CuspidalVisualizer(kinematics, enable_3d=False)
 # viz.update()
-# viz.plot_waypoints(waypoints)
 
 color = 'snow'
-# plot_detJ = plot_implicit(Eq(kinematics.determinant_jacobian(theta2, theta3), 0), 
-#                           x_var=(theta2, -pi, pi), y_var=(theta3, -pi, pi), 
-#                           line_color=color ,show=False)
-# viz.move_sympyplot_to_axes(plot_detJ, viz.ax3)
+plot_detJ = plot_implicit(Eq(kinematics.determinant_jacobian(theta2, theta3), 0), 
+                          x_var=(theta2, -pi, pi), y_var=(theta3, -pi, pi), 
+                          line_color=color ,show=False)
+viz.move_sympyplot_to_axes(plot_detJ, viz.ax3)
 
-# plot_discr = plot_implicit(Eq(kinematics.quartic_discriminant(rho, zee), 0), 
-#                            x_var=(rho, 0, 3.8), y_var=(zee, -2.8, 2.8), 
-#                            line_color=color, show=False, adaptive=False, points=800)
-# viz.move_sympyplot_to_axes(plot_discr, viz.ax2)
+plot_discr = plot_implicit(Eq(kinematics.quartic_discriminant(rho, zee), 0), 
+                           x_var=(rho, 0, 3.8), y_var=(zee, -2.8, 2.8), 
+                           line_color=color, show=False, adaptive=False, points=800)
+viz.move_sympyplot_to_axes(plot_discr, viz.ax2)
 
-# print(kinematics.origins([0, 0, 0]))
-# print(kinematics.forward_kinematics_2D([0, 0, 0]))
+print(kinematics.origins([0, 0, 0]))
+print(kinematics.forward_kinematics_2D([0, 0, 0]))
 
-# viz.pause(2)
+viz.pause(2)
 
 xee = 1.2
 yee = 1.
@@ -61,52 +60,53 @@ zee = 0.5
 #####################
 
 ## INTERPOLATE FROM ONE POSTURE TO ANOTHER
-all_solns = kinematics.ik(xee, yee, zee)
-interpolated_angles = np.linspace(all_solns[0], all_solns[2], num=40)
-print(interpolated_angles)
+# all_solns = kinematics.ik(xee, yee, zee)
+# interpolated_angles = np.linspace(all_solns[0], all_solns[2], num=40)
+# print(interpolated_angles)
 
 # viz.pause(2)
 
-for i in range(len(interpolated_angles)):
-    joints = kinematics.random_valid_config()
-    angles = interpolated_angles[i]
-    viz.update(angles)
+# for i in range(len(interpolated_angles)):
+#     joints = kinematics.random_valid_config()
+#     angles = interpolated_angles[i]
+#     viz.update(angles)
 #####################
 
 ## INTERPOLATE TWO WORKSPACE PATHS TO SHOW JOINT JUMPS
-# x_s = 2.45
-# y_s = 0.7
-# z_s = 0.2
-# x_e = 0.3
-# y_e = 0.7
-# z_e = 0.2
+x_s = 2.45
+y_s = 0.7
+z_s = 0.2
+x_e = 0.3
+y_e = 0.7
+z_e = 0.2
 
 
-# interp_rhozee = np.linspace(np.array([x_s, y_s, z_s]), np.array([x_e, y_e, z_e]), num=40)
+interp_rhozee = np.linspace(np.array([x_s, y_s, z_s]), np.array([x_e, y_e, z_e]), num=40)
 
-# all_solns = kinematics.ik(x_s, y_s, z_s)
-# configs = [all_solns[0]]
-# norms = []
+all_solns = kinematics.ik(x_s, y_s, z_s)
+configs = [all_solns[1]]
+norms = []
 
-# viz.pause(2)
+viz.pause(2)
 
-# for x, y, z in interp_rhozee:
-#     postures = kinematics.ik(x, y, z)
-#     print("number of postures found: {}".format(len(postures)))
-#     # pick the closest config
-#     best_norm = np.inf
-#     best_config = None
-#     for posture in postures:
-#         norm = sum(np.linalg.norm(np.vstack((configs[-1], posture)), axis=0))
-#         if norm < best_norm:
-#             best_norm = norm
-#             best_config = posture
-#     configs.append(best_config)
-#     norms.append(best_norm)
+for x, y, z in interp_rhozee:
+    postures = kinematics.ik(x, y, z)
+    print("number of postures found: {}".format(len(postures)))
+    # pick the closest config
+    best_norm = np.inf
+    best_config = None
+    for posture in postures:
+        norm = sum(np.linalg.norm(np.vstack((configs[-1], posture)), axis=0))
+        if norm < best_norm:
+            best_norm = norm
+            best_config = posture
+    configs.append(best_config)
+    norms.append(best_norm)
 
-# for config in configs:
-#     viz.update(config)
-#     viz.pause(0.15)
+for config in configs:
+    print("updating viz")
+    viz.update(config)
+    viz.pause(0.15)
 
 ######################################################
 
--- viz_manip.py
+++ viz_manip.py
@@ -4,6 +4,7 @@ Methods for visualizing the a robot arm and trajectories
 @author: Achille
 """
 import matplotlib.pyplot as plt
+import matplotlib.pylab as pylab
 from matplotlib import gridspec
 from sympy.plotting import plot_implicit
 from mpl_toolkits.mplot3d import Axes3D
@@ -12,9 +13,9 @@ from skimage import measure
 from mayavi import mlab
 from pyface.api import GUI
 
+
 plt.style.use('seaborn-dark')
 
-import matplotlib.pylab as pylab
 params = {'legend.fontsize': 'x-large',
          'axes.labelsize': 'large',
          'axes.titlesize':'x-large',
@@ -25,15 +26,17 @@ pylab.rcParams.update(params)
 
 class CuspidalVisualizer:
 
-    def __init__(self, kinematics_model):
+    def __init__(self, kinematics_model, enable_3d=True):
         self.kinematics = kinematics_model
+        self.enable_3d = enable_3d
 
-        try:
-            self.engine = mayavi.engine
-        except NameError:
-            from mayavi.api import Engine
-            self.engine = Engine()
-            self.engine.start()
+        if self.enable_3d:
+            try:
+                self.engine = mayavi.engine
+            except NameError:
+                from mayavi.api import Engine
+                self.engine = Engine()
+                self.engine.start()
 
         plt.ion()
         self.fig = plt.figure(figsize=(18,10))
@@ -45,15 +48,15 @@ class CuspidalVisualizer:
         gs = gridspec.GridSpec(1, 3, width_ratios = [3, 1, 1]) 
         gs.update(wspace=0.1, hspace=0.05) # set the spacing between axes. 
 
-        # self.ax = plt.subplot(gs[0], projection='3d')
-        self.ax = plt.subplot(gs[0])
+        self.ax = plt.subplot(gs[0], projection='3d')
+        # self.ax = plt.subplot(gs[0])
         self.ax2 = plt.subplot(gs[1])
         self.ax3 = plt.subplot(gs[2])
 
-        ax1_xr = (-0.3, 3.7)
-        ax1_yr = (-2.2, 2)
-        ax1_zr = (0, 2.5)
-        resoln = 0.05
+        ax1_xr = (-0.6, 3)
+        ax1_yr = (-2, 2)
+        ax1_zr = (-0.05, 1)
+        resoln = 0.02
 
         xl = np.linspace(ax1_xr[0], ax1_xr[1], int(np.diff(ax1_xr)[0]/resoln))
         yl = np.linspace(ax1_yr[0], ax1_yr[1], int(np.diff(ax1_yr)[0]/resoln))
@@ -69,42 +72,46 @@ class CuspidalVisualizer:
         # plot_trisurf(X, Y, triangles)
         # self.ax.plot_trisurf(verts[:, 1], verts[:, 0], faces, verts[:, 2], cmap='inferno', lw=0, alpha=0.7)
 
-        mlab.triangular_mesh(verts[:, 0], verts[:, 1], verts[:, 2], faces, opacity=0.7, transparent=True)
-        self.mfig = mlab.gcf()
-        # mlab.show()
+        if self.enable_3d:
+            mlab.triangular_mesh(verts[:, 0], verts[:, 1], verts[:, 2], faces, opacity=0.7, transparent=True)
+            module_manager = self.engine.scenes[0].children[0].children[0].children[0]
+            module_manager.scalar_lut_manager.lut_mode = 'summer'
+            self.mfig = mlab.gcf()
+            # mlab.show()
 
 
-        # self.ax.set_autoscale_on(False)
-        # self.ax.set_xlim3d(ax1_xr)
-        # self.ax.set_ylim3d(ax1_yr)
-        # self.ax.set_zlim3d(ax1_zr)
-        # self.ax.set_aspect('auto')
-        # self.ax.set_xlabel('$X$')
-        # self.ax.set_ylabel('$Y$')
-        # self.ax.set_zlabel('$Z$')
-        # self.ax.set_facecolor(self.color1)
+        self.ax.set_autoscale_on(False)
+        self.ax.set_xlim3d(ax1_xr)
+        self.ax.set_ylim3d(ax1_yr)
+        self.ax.set_zlim3d(ax1_zr)
+        self.ax.set_aspect('auto')
+        self.ax.set_xlabel('$X$')
+        self.ax.set_ylabel('$Y$')
+        self.ax.set_zlabel('$Z$')
+        self.ax.set_facecolor(self.color1)
         # self.ax.set_proj_type('ortho')
-        # self.ax2.set_facecolor(self.color1)
-        # self.ax3.set_facecolor(self.color1)
-        # self.ax2.yaxis.label.set_color(self.axis_color)
-        # self.ax3.yaxis.label.set_color(self.axis_color)
-        # self.ax2.xaxis.label.set_color(self.axis_color)
-        # self.ax3.xaxis.label.set_color(self.axis_color)
+        self.ax2.set_facecolor(self.color1)
+        self.ax3.set_facecolor(self.color1)
+        self.ax2.yaxis.label.set_color(self.axis_color)
+        self.ax3.yaxis.label.set_color(self.axis_color)
+        self.ax2.xaxis.label.set_color(self.axis_color)
+        self.ax3.xaxis.label.set_color(self.axis_color)
 
-        # self.ax2.spines['bottom'].set_color(self.axis_color)
-        # self.ax3.spines['bottom'].set_color(self.axis_color)
-        # self.ax2.spines['bottom'].set_color(self.axis_color)
-        # self.ax3.spines['left'].set_color(self.axis_color)
-        # self.ax2.tick_params(colors=self.axis_color)
-        # self.ax3.tick_params(colors=self.axis_color)
+        self.ax2.spines['bottom'].set_color(self.axis_color)
+        self.ax3.spines['bottom'].set_color(self.axis_color)
+        self.ax2.spines['bottom'].set_color(self.axis_color)
+        self.ax3.spines['left'].set_color(self.axis_color)
+        self.ax2.tick_params(colors=self.axis_color)
+        self.ax3.tick_params(colors=self.axis_color)
 
-        # plt.tight_layout()
+        plt.tight_layout()
         plt.show()
 
         self.joint_angles = np.zeros(3)
         self.plotted_lines = []
         self.skip = 0
         self.angle = -190
+        self.ax.view_init(8, self.angle)
         # self.update()
 
     def update(self, joint_angles=np.zeros(3)):
@@ -116,43 +123,48 @@ class CuspidalVisualizer:
             else:
                 self.skip += 1
         except IndexError: pass
+
         if self.skip <= 2:
-            # self.plotted_lines.append(self.ax.plot(origins[:, 0], origins[:, 1], origins[:, 2], '-o', color=self.color2, linewidth=5, markersize=10))
-            mlab.plot3d(origins[:, 0], origins[:, 1], origins[:, 2], figure=self.mfig)
-            mlab.points3d(origins[:, 0], origins[:, 1], origins[:, 2], figure=self.mfig, scale_factor=0.1)
+            self.plotted_lines.append(self.ax.plot(origins[:, 0], origins[:, 1], origins[:, 2], '-o', color=self.color2, linewidth=5, markersize=8, alpha=1.0))
+            # mlab.plot3d(origins[:, 0], origins[:, 1], origins[:, 2], figure=self.mfig)
+            # mlab.points3d(origins[:, 0], origins[:, 1], origins[:, 2], figure=self.mfig, scale_factor=0.1)
         # self.plotted_lines.append(mlab.plot3d(origins[:, 0], origins[:, 1], origins[:, 2], figure=self.mfig))
         elif self.skip > 2:
             # color the previous line in a different color
-            last_point = self.engine.scenes[0].children[-1].children[0].children[0]
-            last_line = self.engine.scenes[0].children[-2].children[0].children[0].children[0].children[0]
-            last_line.actor.property.opacity = 0.15
-            last_point.actor.property.opacity = 0.15
+            # last_point = self.engine.scenes[0].children[-1].children[0].children[0]
+            # last_line = self.engine.scenes[0].children[-2].children[0].children[0].children[0].children[0]
+            # last_line.actor.property.opacity = 0.15
+            # last_point.actor.property.opacity = 0.15
                 # line.set_color('lightskyblue')
                 # line.set_alpha(0.4)
             #     line.set_linewidth(3)
             #     line.set_markersize(6)
-            mlab.plot3d(origins[:, 0], origins[:, 1], origins[:, 2], figure=self.mfig)
-            mlab.points3d(origins[:, 0], origins[:, 1], origins[:, 2], figure=self.mfig, scale_factor=0.1)
-            # self.plotted_lines.append(self.ax.plot(origins[:, 0], origins[:, 1], origins[:, 2], '-o', color=self.color2, linewidth=5, markersize=10))
+            # mlab.plot3d(origins[:, 0], origins[:, 1], origins[:, 2], figure=self.mfig)
+            # mlab.points3d(origins[:, 0], origins[:, 1], origins[:, 2], figure=self.mfig, scale_factor=0.1)
+            for prev_lines in self.plotted_lines[-min(5, len(self.plotted_lines)):]:
+                for link in prev_lines:
+                    link.set_alpha(max(0, link.get_alpha() - 0.15))
+                    link.set_linewidth(1)
+            self.plotted_lines.append(self.ax.plot(origins[:, 0], origins[:, 1], origins[:, 2], '-o', color=self.color2, linewidth=2, markersize=8, alpha=1.0))
 
         # set the ee color
-        # self.ax.plot(origins[-1, 0], origins[-1, 1], origins[-1, 2], 'o', color=self.color3, markersize=5)
+        self.ax.plot(origins[-1, 0], origins[-1, 1], origins[-1, 2], 'o', color=self.color3, markersize=5)
 
         # show where the robot is on the other plots
         rho, zee = self.kinematics.forward_kinematics_2D(joint_angles)
-        # self.ax2.plot([rho], [zee], marker='x', markersize=10, color=self.color3)
-        # self.ax3.plot([joint_angles[1]], [joint_angles[2]], marker='x', markersize=10, color=self.color3)
+        self.ax2.plot([rho], [zee], marker='x', markersize=10, color=self.color3)
+        self.ax3.plot([joint_angles[1]], [joint_angles[2]], marker='x', markersize=10, color=self.color3)
 
         # rotate robot view
-        # self.angle -= 0.2
-        # self.ax.view_init(8, self.angle)
+        self.angle -= 0.2
+        self.ax.view_init(8, self.angle)
 
-        GUI().process_events()
+        # GUI().process_events()
         # arr = mlab.screenshot()
         # self.ax.imshow(arr)
-        # self.fig.canvas.draw()
+        self.fig.canvas.draw()
         # mlab.draw(figure=self.mfig)
-        # plt.pause(0.001)  # tiny delay to allow for visualizing
+        plt.pause(0.001)  # tiny delay to allow for visualizing
 
     def plot_waypoints(self, target_poses, style='arrow'):
         """
